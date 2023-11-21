f2a71cf72d99d414cd5ad26881dcd4238ca77165
Aram Arakelyan <aram777arakelyan@gmail.com>
2023-11-21 15:38:25 +0400

    called the function onClick-ing the buttons

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -5,26 +5,26 @@ import { useState } from "react";
 
 function QuizPage() {
     const navigate = useNavigate();
-    const locaton = useLocation();
+    const location = useLocation();
 
-    let currentQuestions, score;
+    let currentQuestions;
 
-    if (locaton.state === "music") currentQuestions = musicQuestions;
-    else if (locaton.state === "movies") currentQuestions = moviesQuestions;
-    else if (locaton.state === "maths") currentQuestions = mathsQuestions;
-    else if (locaton.state === "geography") currentQuestions = geographyQuestions;
-    else if (locaton.state === "history") currentQuestions = historyQuestions;
+    if (location.state === "music") currentQuestions = musicQuestions;
+    else if (location.state === "movies") currentQuestions = moviesQuestions;
+    else if (location.state === "maths") currentQuestions = mathsQuestions;
+    else if (location.state === "geography") currentQuestions = geographyQuestions;
+    else if (location.state === "history") currentQuestions = historyQuestions;
     else console.log("error");
 
     const navigateToResult = (currentQuestionsLength) => {
         navigate('/result', { state: { score, currentQuestionsLength } });
     };
 
-    let [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [score, setScore] = useState(0);
 
     const handleAnswerClick = (selectedVariant) => {
-        const isCorrect = currentQuestions[currentIndex].trueVariant === selectedVariant;
-        if (isCorrect) score = (score || 0) + 1;
+        if (selectedVariant === currentQuestions[currentIndex].trueVariant) setScore(score + 1);
 
         if (currentIndex < currentQuestions.length - 1) setCurrentIndex(currentIndex + 1);
         else navigateToResult(currentQuestions.length);
@@ -34,25 +34,10 @@ function QuizPage() {
         <div>
             <h1>{currentQuestions[currentIndex].question}</h1>
             <div id="quizPageButtons">
-                <button onClick={() => {
-                    if (currentIndex < currentQuestions.length - 1) setCurrentIndex(currentIndex + 1);
-                    else navigateToResult(currentQuestions.length);
-                }}>{currentQuestions[currentIndex].variant1}</button>
-
-                <button onClick={() => {
-                    if (currentIndex < currentQuestions.length - 1) setCurrentIndex(currentIndex + 1);
-                    else navigateToResult(currentQuestions.length);
-                }}>{currentQuestions[currentIndex].variant2}</button>
-
-                <button onClick={() => {
-                    if (currentIndex < currentQuestions.length - 1) setCurrentIndex(currentIndex + 1);
-                    else navigateToResult(currentQuestions.length);
-                }}>{currentQuestions[currentIndex].variant3}</button>
-
-                <button onClick={() => {
-                    if (currentIndex < currentQuestions.length - 1) setCurrentIndex(currentIndex + 1);
-                    else navigateToResult(currentQuestions.length);
-                }}>{currentQuestions[currentIndex].variant4}</button>
+                <button onClick={() => handleAnswerClick(1)}>{currentQuestions[currentIndex].variant1}</button>
+                <button onClick={() => handleAnswerClick(2)}>{currentQuestions[currentIndex].variant2}</button>
+                <button onClick={() => handleAnswerClick(3)}>{currentQuestions[currentIndex].variant3}</button>
+                <button onClick={() => handleAnswerClick(4)}>{currentQuestions[currentIndex].variant4}</button>
             </div>
         </div>
     );
